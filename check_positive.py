255a8e4bd7967ea6f85ddc4fd2660de3e11ccfd0
Luke Dyer <luke.dyer@student.uq.edu.au>
2025-04-06 23:25:49 +0000

    test

--- check_positive.py
+++ check_positive.py
@@ -1,4 +1,3 @@
-LUKE->-Tralalero
 # This script defines a function to check if a number is positive.
 def is_positive(number):
     if number > 0:
@@ -11,4 +10,3 @@ test_number = 10
 output = is_positive(test_number)
 print(f"Is the number positive? {output}")
 
-main
